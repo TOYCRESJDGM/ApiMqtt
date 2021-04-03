be1b17d0fad5d5eeac612d339055a53d53fc183c
Wilmer David Guarin Gutierrz <67870545+wilmer1022@users.noreply.github.com>
2021-04-03 10:30:56 -0500

    Feature/vallesoft 119 (#9)
    * consumo API para crear bodega
    
    * manejo de alertas para crear tipo de articulo

--- src/Components/ArticleType/CreateArticleType.js
+++ src/Components/ArticleType/CreateArticleType.js
@@ -1,6 +1,8 @@
 import React, { Component} from 'react'
 import './Styles.css'
 
+import Alert from '../Alerts/Alert'
+
 class CreateArticleType extends Component {
   constructor() {
     super() 
@@ -8,7 +10,7 @@ class CreateArticleType extends Component {
       name: '',
       desc: '',
       classif: '',
-      is_parent: false
+      is_parent: false,
     }
     this.handleInputChange = this.handleInputChange.bind(this);
   }
@@ -23,9 +25,77 @@ class CreateArticleType extends Component {
     })
   }
 
+  handleChange = (event) => {
+    let attribute = event.target.id
+    let value = event.target.value
+
+    this.setState({ [attribute]: value })
+  }
+
+  clearInputs = () => {
+    this.setState({
+      name: '',
+      desc: '',
+      classif: '',
+      is_parent: false,
+    })
+
+    return
+  }
+
+  // Functions to handle alerts
+  close = () => {
+    this.setState({ alert: '' })
+  }
+
+  buildAlert(type, text) {
+    return <Alert type={type} text={text} close={this.close} />
+  }
+
+  createArticleType = () => {
+    // Verify that the required fields are filled
+    if (!this.checkMandatoryInputs()) {
+      this.setState({
+        alert: this.buildAlert(
+          'attention',
+          'Verifique que ha llenado todos los campos obligatorios.'
+        ),
+      })
+
+      return
+    }
+
+    let body = {
+      article_type_name: this.state.name,
+      desc: this.state.desc,
+      classif: this.state.classif,
+      is_parent: this.state.is_parent
+    }
+
+    this.setState({
+      alert: this.buildAlert(
+        'success',
+        'Se han llenado todos los campos obligatorios.'
+      ),
+    })
+  }
+
+  checkMandatoryInputs() {
+    if (!this.state.name) {
+      return false
+    }
+
+    if (!this.state.classif) {
+      return false
+    }
+
+    return true
+  }
+
   render() {
     return(
-	  <div className='ca-container'>
+	   <div className='ca-container'>
+      {this.state.alert}
         <span className='global-comp-title'>Crear tipo de artículo</span>
         <span className='global-comp-description'>
           Diligencie el formulario para crear un tipo de artículo. Los campos
@@ -37,21 +107,36 @@ class CreateArticleType extends Component {
               Nombre
               <strong className='global-form-mandatory'> *</strong>
             </span>
-            <input className='global-form-input' />
+            <input
+            id='name'
+            type='text'
+            value={this.state.name}
+            onChange={this.handleChange}
+            className='global-form-input' />
           </div>
           <div className='global-form-group'>
             <span className='global-form-label'>
               Descripción
             </span>
-            <input className='global-form-input' />
+            <input
+            id='desc'
+            type='text'
+            value={this.state.desc}
+            onChange={this.handleChange}
+            className='global-form-input' />
           </div>
           <div className='global-form-group'>
             <span className='global-form-label'>
               Categoría
               <strong className='global-form-mandatory'> *</strong>
             </span>		
-			      <select className='global-form-input-select'>
-              <option className='global-form-input-select-option' value='#'>Seleccione uno</option>
+			      <select
+            id='classif'
+            type='text'
+            value={this.state.classif}
+            onChange={this.handleChange}
+            className='global-form-input-select'>
+              <option className='global-form-input-select-option' value=''>Seleccione uno</option>
               <option className='global-form-input-select-option' value='Elementos de cocina'>Elementos de cocina</option>
               <option className='global-form-input-select-option' value='Elementos de limpieza'>Elementos de limpieza</option>
               <option className='global-form-input-select-option' value='Elementos para acampar'>Elementos para acampar</option>
@@ -69,8 +154,12 @@ class CreateArticleType extends Component {
             </span>
           </div>
           <div className='global-form-buttons-container'>
-            <button className='global-form-solid-button'>Enviar</button>
-            <button className='global-form-outline-button'>Cancelar</button>
+            <button
+            onClick={this.createArticleType}
+            className='global-form-solid-button'>Enviar</button>
+            <button
+            onClick={this.clearInputs}
+            className='global-form-outline-button'>Cancelar</button>
           </div>
         </div>
       </div> 
