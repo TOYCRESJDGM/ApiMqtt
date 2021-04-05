df1e684f3c8f7f6747c2d4bbd78e9ec74ce0b5a6
vargas-ins <vargas.insuasti31@gmail.com>
2021-04-05 14:11:55 -0500

    Add constant and a function to process generic select options

--- src/Functions/Helpers.js
+++ src/Functions/Helpers.js
@@ -2,3 +2,21 @@ export function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
 }
+
+export function setSelectOptions(options) {
+  if (options.length < 1) {
+    return
+  }
+
+  let select_options = []
+
+  for (op in options) {
+    select_options.push(
+      <option className='global-form-input-select-option' value={op.value}>
+        {op.name}
+      </option>
+    )
+  }
+
+  return select_options
+}
